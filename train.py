37ae970b67b93e4c73ead39933647deff9a8baf6
Baiyuan Chen <chenbaiyuan75@gmail.com>
2025-10-31 20:10:20 +0900

    .

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 
-from model1 import GPTConfig, GPT
+from model import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
